0de189c79bda1551f9afaae4e8b8557f0044afe1
HamburgerEater <36900971+HamburgerEater@users.noreply.github.com>
2019-04-11 22:00:03 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,14 +1,6 @@
 <html>
 <body>
 
-  <form action="action_page.php method="post">
-    Name:<br>
-    <input type="text" name="foodName"><br> 
-    Expiry Date:<br>
-    <input type="text" name="expDate"><br>
-    <input type="submit" value="Submit">
-  </form>
-
  <?php
   echo $_POST["foodName"];
  ?>
